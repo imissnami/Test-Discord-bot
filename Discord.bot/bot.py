c4922a8d5ba0fcb63b88f1785be75cb1f8349204
imissnami <118085775+imissnami@users.noreply.github.com>
2022-11-15 18:33:58 -0500

    Add files via upload

---
+++ Discord.bot/bot.py
@@ -0,0 +1,33 @@
+from discord.ext import commands
+import discord
+
+intents = discord.Intents.all()
+intents.members = True
+
+bot = commands.Bot(command_prefix=".", help_command= None, intents = intents)
+
+@bot.event
+async def on_ready():
+    print("this bot in now online")
+
+@bot.command()
+async def say(ctx):
+    embed=discord.Embed(title="Mazza", description="description", color=0xff0000)
+    embed.set_footer(text="Power By Mazza")
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def test(ctx):
+    await ctx.send("Mazza")
+
+#bot.event
+#sync def on_message(message):
+#   Blacklisted = [".com", "https://", "http://", "www.", "discord.gg", ".gif"]
+#   report_logs = bot.get_channel(1041550283471343686)
+#   for m in Blacklisted: 
+#       if m in message.content or m.lower() in message.content: 
+#           await report_logs.send(f"{message.author.mention} has tried to use blacklisted word.")
+#           await message.delete()
+#           await message.channel.send(f"You can not send this message {message.author.mention}.")
+
+bot.run("MTA0MTQ4NzcwNTM4Njc4Mjc0MA.GxuFzD.Qslh7AMhoGObb-tMbKZgr1bpCd6Cc4xzISOBNQ")
